08f49c867541c5fd802b1756fc95fb24ab12b572
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-22 15:52:15 +0200

    ⠀⠀⠀⠀⠀

--- Editor/UIMenuEditor.cs
+++ Editor/UIMenuEditor.cs
@@ -1,69 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEditor;
-using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 namespace UnityEssentials
 {
-    class SimpleTreeView : TreeView
-    {
-        private List<TreeViewItem> _items;
-        public SimpleTreeView(TreeViewState state, List<TreeViewItem> items) : base(state)
-        {
-            _items = items;
-        }
-
-        protected override TreeViewItem BuildRoot()
-        {
-            var root = new TreeViewItem { id = 0, depth = -1, displayName = "Root" };
-            SetupParentsAndChildrenFromDepths(root, _items);
-            return root;
-        }
-
-        protected override bool CanStartDrag(CanStartDragArgs args)
-        {
-            return true;
-        }
-
-        protected override void SetupDragAndDrop(SetupDragAndDropArgs args)
-        {
-            if (hasSearch)
-                return;
-
-            DragAndDrop.PrepareStartDrag();
-            var draggedRows = GetRows().Where(item => args.draggedItemIDs.Contains(item.id)).ToList();
-            DragAndDrop.objectReferences = new UnityEngine.Object[] { }; // Required, but not used
-            DragAndDrop.SetGenericData("TreeViewDragging", draggedRows);
-            DragAndDrop.StartDrag("Dragging TreeView");
-        }
-
-        protected override DragAndDropVisualMode HandleDragAndDrop(DragAndDropArgs args)
-        {
-            var draggedRows = DragAndDrop.GetGenericData("TreeViewDragging") as List<TreeViewItem>;
-            if (draggedRows == null)
-                return DragAndDropVisualMode.None;
-
-            if (args.performDrop)
-            {
-                foreach (var draggedItem in draggedRows)
-                    _items.RemoveAll(i => i.id == draggedItem.id);
-
-                int insertIndex = args.parentItem == null || args.parentItem.id == 0
-                    ? args.insertAtIndex
-                    : _items.FindIndex(i => i.id == args.parentItem.id) + 1;
-
-                if (insertIndex < 0 || insertIndex > _items.Count)
-                    insertIndex = _items.Count;
-
-                _items.InsertRange(insertIndex, draggedRows);
-                Reload();
-            }
-            return DragAndDropVisualMode.Move;
-        }
-    }
-
     public class UIMenuEditor
     {
         public EditorWindowDrawer Window;
@@ -71,8 +12,6 @@ namespace UnityEssentials
         public Action Close;
 
         private UIMenuData _data = new();
-        private TreeViewState _treeViewState;
-        private SimpleTreeView _treeView;
         public List<ScriptableObject> Root = new();
 
         [MenuItem("Tools/ UI Menu Builder %g", false, priority = 1003)]
@@ -105,19 +44,18 @@ namespace UnityEssentials
             GUILayout.FlexibleSpace();
         }
 
-        private List<TreeViewItem> _treeData = new()
+        private List<SimpleTreeViewItem> _treeData = new()
         {
-            new TreeViewItem { id = 1, depth = 0, displayName = "Category 1" },
-            new TreeViewItem { id = 2, depth = 1, displayName = "Header 1" },
-            new TreeViewItem { id = 3, depth = 0, displayName = "Category 2" },
+            new SimpleTreeViewItem { id = 1, depth = 0, displayName = "Category 1" },
+            new SimpleTreeViewItem { id = 2, depth = 1, displayName = "Header 1" },
+            new SimpleTreeViewItem { id = 3, depth = 0, displayName = "Category 2" },
         };
 
+        private SimpleTreeView _treeView;
         private void Pane()
         {
-            _treeView ??= new SimpleTreeView(_treeViewState ??= new(), _treeData);
-            var rect = GUILayoutUtility.GetRect(0, float.MaxValue, 0, float.MaxValue, GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true));
-            _treeView.Reload();
-            _treeView.OnGUI(rect);
+            _treeView ??= new SimpleTreeView(_treeData);
+            _treeView.OnGUI();
         }
 
         private void Body()
