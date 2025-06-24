339b6bda3a538b274a604f24f0633f4704d9d3eb
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2025-06-24 02:42:44 +0200

    ⠀⠀⠀⠀⠀

--- Editor/Helper/CustomScriptableObjectDrawer.cs
+++ Editor/Helper/CustomScriptableObjectDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEditorInternal;
@@ -126,3 +127,4 @@ namespace UnityEssentials
         }
     }
 }
+#endif
--- Editor/UIMenuEditorUtilities.cs
+++ Editor/UIMenuEditorUtilities.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 
@@ -119,3 +120,4 @@ namespace UnityEssentials
 
     }
 }
+#endif
